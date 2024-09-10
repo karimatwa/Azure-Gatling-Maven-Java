2d9d0a812235dce67e8c2431548d76495b545cd1
Karim Atwa <karim-atwa@hotmail.com>
2024-09-11 00:00:06 +0200

    Update SimulationName.java

--- src/test/java/packagename/SimulationName.java
+++ src/test/java/packagename/SimulationName.java
@@ -10,6 +10,7 @@ public class SimulationName extends Simulation {
 
   public static final int vu = Integer.getInteger("vu", 1);
 
+  
   HttpProtocolBuilder httpProtocol =
       http.baseUrl("https://computer-database.gatling.io")
           .acceptHeader("application/json")
